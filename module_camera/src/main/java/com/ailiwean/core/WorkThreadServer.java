ab91065d3adf6a3411e2ff42bb428e96b2e37a36
aiWean <1581209979@qq.com>
2020-05-09 17:03:32 +0800

    调整线程池数量提升扫码速度

--- module_camera/src/main/java/com/ailiwean/core/WorkThreadServer.java
+++ module_camera/src/main/java/com/ailiwean/core/WorkThreadServer.java
@@ -2,6 +2,7 @@ package com.ailiwean.core;
 
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +30,15 @@ public class WorkThreadServer {
     private WorkThreadServer() {
     }
 
+    //参数初始化
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    //核心线程数量大小
+    private static final int corePoolSize = Math.max(2, Math.min(CPU_COUNT - 1, 4));
+    //线程池最大容纳线程数
+    private static final int maximumPoolSize = CPU_COUNT * 2 + 1;
+    //线程空闲后的存活时长
+    private static final int keepAliveTime = 30;
+
     private static class Holder {
         static WorkThreadServer INSTANCE = new WorkThreadServer();
     }
@@ -36,8 +46,8 @@ public class WorkThreadServer {
     public static WorkThreadServer getInstance() {
         if (Holder.INSTANCE.executor == null) {
             Holder.INSTANCE.executor = new ThreadPoolExecutor(
-                    2, 5, 1, TimeUnit.SECONDS,
-                    new ArrayBlockingQueue<>(20, true), new ThreadPoolExecutor.DiscardOldestPolicy());
+                    corePoolSize, corePoolSize, keepAliveTime, TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<>(maximumPoolSize, true), new ThreadPoolExecutor.DiscardOldestPolicy());
         }
         if (Holder.INSTANCE.handlerThread == null) {
             Holder.INSTANCE.handlerThread = new HandlerThread("Default");
